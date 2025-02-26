a03f505deca0fe11afce1ceb9d87b0b26a27391b
Belka13v <victory27b@gmail.com>
2025-02-26 11:32:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double f = 0;
+            int a = 0;
+            string s=null ;
+            //StreamReader streamRdrr=null;
+            //try
+            //{
+            //    streamRdrr = new StreamReader("info.txt");
+            //    f = double.Parse(streamRdrr.ReadLine());
+            //    a = int.Parse(streamRdrr.ReadLine());
+            //    s = streamRdrr.ReadLine();
+
+            //}catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    if (streamRdrr != null)
+            //    {
+            //        streamRdrr.Close();
+            //    }
+            //}
+
+            using (StreamReader streamRdrr = new StreamReader("info.txt"))
+            {
+                //try
+                //{
+                    f = double.Parse(streamRdrr.ReadLine());
+                    a = int.Parse(streamRdrr.ReadLine());
+                    s = streamRdrr.ReadLine();
+
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex.Message);
+                //    if (Math.Abs(f - 1E-4) < 1E-3) f = 2.0 / 157;
+                //    if (a == 0) a = 5;
+                //    if (s == null) s = "My String";
+
+                //}
+            }
+
+
+            using (StreamWriter streamWriter = new StreamWriter("info.txt", true))
+            {
+                streamWriter.WriteLine("Hello, World!");
+                Console.WriteLine("Hello, World!");
+                streamWriter.WriteLine($"{f,17:f5}!!!!!!!!!");
+                Console.WriteLine($"{f,17:f5}!!!!!!!!!");
+            }
+            //streamWriter.Close();
+            Console.ReadKey();
+        }
+    }
+}
